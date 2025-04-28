6b85fb11ed815c33c564897a7083c3c3110a03fc
BGCho96 <73811730+BGCho96@users.noreply.github.com>
2025-04-28 18:01:28 +0900

    fetch log added

--- tests/dags/my_tasks.py
+++ tests/dags/my_tasks.py
@@ -64,9 +64,8 @@ def validate_data():
     ...
 
 def insert_data(**context):
-    # XCom에서 JSON 불러오기
     ti = context["ti"]
-    raw_json = context['ti'].xcom_pull(task_ids='fetch_raw_data', key='raw_df')
+    raw_json = ti.xcom_pull(task_ids='fetch_raw_data', key='raw_df')
     df = pd.read_json(raw_json)
 
     # 전처리
@@ -80,7 +79,8 @@ def insert_data(**context):
 
     # 전역 None/결측 처리
     df = df.replace([pd.NaT,'', ' ', 'null', 'N/A'], None)
-    # df = df.where(pd.notnull(df), None)
+
+    print(df.head(10))
 
     # DB 접속 정보
     db_url = "postgresql+psycopg2://postgres:postgres@airflow-postgresql.airflow:5432/postgres"
@@ -92,81 +92,10 @@ def insert_data(**context):
         host=url.host,
         port=url.port
     )
-    cur = conn.cursor()
 
-    columns = [
-        "자치구코드", "법정동코드", "계약일", "취소일", "지번구분", "지번구분명",
-        "본번", "부번", "건물명", "층", "건물면적(㎡)", "토지면적(㎡)", "물건금액(만원)",
-        "건물용도", "건축년도", "신고구분", "권리구분", "접수연도", "신고한 개업공인중개사 시군구명"
-    ]
-
-    insert_sql = f"""
-        INSERT INTO 부동산데이터 ({', '.join(f'"{col}"' for col in columns)})
-        VALUES ({', '.join(['%s'] * len(columns))})
-    """
-
-    success_count = 0
-    fail_count = 0
     bulk_insert_data(df, "부동산데이터", conn, chunk_size=100)
 
-    # for i, row in df.iterrows():
-    #     values = [
-    #     None if pd.isna(row.get(col, None)) else row.get(col, None)
-    #     for col in columns
-    # ]
-    #     sql_to_print = cur.mogrify(insert_sql, values).decode('utf-8')
-    #     print(f"[SQL Preview] {sql_to_print}")
-
-    #     print(values)
-    #     try:
-    #         cur.execute(insert_sql, values)
-    #         success_count += 1
-    #         print(f"[✅ row {i}] 삽입 성공")
-    #     except Exception as e:
-    #         conn.rollback()
-    #         fail_count += 1
-    #         print(f"[❌ row {i}] 삽입 실패: {e}")
-
-    # conn.commit()
-    # cur.close()
-    # conn.close()
-    # print(f"[결과 요약] 성공: {success_count}, 실패: {fail_count}, 전체: {len(df)}")
-
-
-
-    # # XCom에서 JSON 불러오기
-    # ti = context["ti"]
-    # raw_json = context['ti'].xcom_pull(task_ids='fetch_raw_data', key='raw_df')
-    # df = pd.read_json(raw_json)
-
-    # # 제거할 컬럼
-    # df = df.drop(columns=["자치구명", "법정동명"], errors="ignore")
-    # df["계약일"] = pd.to_datetime(df["계약일"], format="%Y%m%d", errors="coerce")
-    # df["취소일"] = pd.to_datetime(df["취소일"], format="%Y%m%d", errors="coerce")
-    # df["층"] = df["층"].replace(['', ' ', 'null', 'N/A'], np.nan)
-
-    # # 2. nullable integer로 변환 (Int64)
-    # df["층"] = df["층"].astype("Int64")
-    # df = df.replace(['', ' ', 'null', 'N/A'], None)
-
-
-    # # DB 연결 (SQLAlchemy)
-    # db_url = "postgresql+psycopg2://airflow:airflow@postgres:5432/airflow"
-    # engine = create_engine(db_url)
-    # # df = df.replace(['', ' ', 'null', 'N/A'], np.nan).where(pd.notnull(df), None)
-    # # 테이블에 데이터 append (존재하는 테이블만 대상, CREATE는 안함)
-    # df.to_sql(
-    #     name="부동산데이터",
-    #     con=engine,
-    #     if_exists="append",   # append: 행 추가만 수행
-    #     index=False,
-    #     method="multi"        # (optional) 다중 insert 성능 향상
-    # )
-
-    # print("df row count:", len(df))
-
 def prune_old_data():
-    # 3일 초과된 데이터 delete
     db_url = "postgresql+psycopg2://postgres:postgres@airflow-postgresql.airflow:5432/postgres"
     engine = create_engine(db_url)
 
@@ -184,8 +113,8 @@ def t5_check_table():
 
     with engine.connect() as conn:
         df = pd.read_sql("SELECT * FROM 부동산데이터", conn)
-        print("DB 미리보기:")
         print(df.to_string(index=False))
+        
 def bulk_insert_data(df, table_name, conn, chunk_size=100):
     cur = conn.cursor()
 
@@ -218,8 +147,12 @@ def bulk_insert_data(df, table_name, conn, chunk_size=100):
         if (idx + 1) % chunk_size == 0:
             try:
                 sql = insert_prefix + ",\n".join(buffer) + ";"
+
+                print(sql)
                 
                 cur.execute(sql)
+                print(cur.fetchall())
+
                 conn.commit()
                 print(f"[✅ {idx+1}개] 삽입 성공")
                 success_count += len(buffer)
