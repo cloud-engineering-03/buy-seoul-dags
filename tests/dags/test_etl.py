308fc61ffc1b2f9097d85d883e71bcfef63f6e7d
BGCho96 <73811730+BGCho96@users.noreply.github.com>
2025-04-28 19:09:20 +0900

    prune off
    ssibal

--- tests/dags/test_etl.py
+++ tests/dags/test_etl.py
@@ -7,6 +7,6 @@ with DAG("seoul_etl_pipeline", start_date=datetime(2024, 1, 1), schedule_interva
     t1 = PythonOperator(task_id="fetch_raw_data", python_callable=fetch_raw_data)
     t2 = PythonOperator(task_id="validate_data", python_callable=validate_data)
     t3 = PythonOperator(task_id="insert_data", python_callable=insert_data)
-    t4 = PythonOperator(task_id="prune_old_data", python_callable=prune_old_data)
+    # t4 = PythonOperator(task_id="prune_old_data", python_callable=prune_old_data)
     t5 = PythonOperator(task_id="t5_check_table", python_callable=t5_check_table)
-    t0 >> t1 >> t3 >> t4
+    t0 >> t1 >> t3
