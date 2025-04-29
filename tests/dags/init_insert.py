d0450aa101de6eb75780c38cbbea831468b20e8a
BGCho96 <73811730+BGCho96@users.noreply.github.com>
2025-04-29 16:45:34 +0900

    file name fix

--- tests/dags/init_insert.py
+++ tests/dags/init_insert.py
@@ -55,7 +55,7 @@ def insert_init_data():
     gu_df = pd.read_json(os.path.join(curdir,"자치구코드_군구명_매핑.json"))
     sido_df = pd.read_json(os.path.join(curdir,"sido_code.json"))
     dist_df = pd.read_json(os.path.join(curdir,"인접자치구_거리.json"))
-    cgg_station_map_df = pd.read_csv(os.path.join(curdir,"서울지하철_역위치_자치구매핑완료.csv"))
+    cgg_station_map_df = pd.read_csv(os.path.join(curdir,"서울지하철_역위치_자치구매핑완료_서울경기인천.csv"))
     
     with engine.begin() as conn:
         
