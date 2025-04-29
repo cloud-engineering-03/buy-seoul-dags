efeda2757e8892f4992240cba9ec6cd0d5c011a8
BGCho96 <73811730+BGCho96@users.noreply.github.com>
2025-04-29 16:44:06 +0900

    typo check

--- tests/dags/init_insert.py
+++ tests/dags/init_insert.py
@@ -84,7 +84,7 @@ def insert_init_data():
         for _, row in cgg_station_map_df.iterrows():
             
             conn.execute(
-                text("INSERT INTO SUBWAY_CGG_MAPPING (역명, 자치구명) VALUES (:station_name, :cgg_name)"),
-                {"station_name": row["역명"], "cgg_name": row["자치구명"]}
+                text("INSERT INTO SUBWAY_CGG_MAPPING (역명, 자치구코드) VALUES (:station_name, :cgg_name)"),
+                {"station_name": row["역명"], "cgg_name": row["자치구코드"]}
             )
 
